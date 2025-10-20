0afbe9740817de16675637bc7cf80f2d313f2680
sparkRGT <joustinalonzo@hotmail.com>
2025-10-20 16:38:58 -0500

    Implementar manejo de mensajes en el canal FilaVirtual y actualizar el cliente WebSocket para unirse a la fila

--- websocket_ruby/ws_client.py
+++ websocket_ruby/ws_client.py
@@ -1,16 +1,16 @@
-import asyncio
-import websockets
+import websocket
+import json
 
-async def main():
-    uri = "ws://localhost:8080"
-    async with websockets.connect(uri) as ws:
-        print('Connected to', uri)
-        try:
-            while True:
-                msg = await ws.recv()
-                print('Received:', msg)
-        except Exception as e:
-            print('Connection closed:', e)
+ws = websocket.WebSocket()
+ws.connect("ws://localhost:8080")
 
-if __name__ == '__main__':
-    asyncio.run(main())
+data = {
+    "channel": "fila_virtual",
+    "action": "join",
+    "id_cliente": 1,
+    "nombre": "Joustin"
+}
+
+ws.send(json.dumps(data))
+print(ws.recv())
+ws.close()
