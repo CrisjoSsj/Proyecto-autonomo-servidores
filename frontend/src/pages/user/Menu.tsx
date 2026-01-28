1a01489b2858705c373ad7bf8c87f4e36134f000
sparkRGT <joustinalonzo@hotmail.com>
2026-01-27 20:53:05 -0500

    fix(Menu): replace hardcoded API URL with constant import

--- frontend/src/pages/user/Menu.tsx
+++ frontend/src/pages/user/Menu.tsx
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import Navbar from "../../components/user/Navbar";
 import MenuCard from "../../components/user/MenuCard";
 import PiePagina from "../../components/user/PiePagina";
+import { API_BASE_URL } from "../../config/constants";
 import "../../css/user/Menu.css";
-
-const API_BASE_URL = 'http://127.0.0.1:8004';
 
 interface Plato {
   id_plato: number;
